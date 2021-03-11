4607d27f2e69fbab3206dcb2c7e5899ec7538d5c
Mike-Like11 <mike7677877@gmail.com>
2021-03-11 19:31:13 +0300

    agaga

--- PhotoPicker/ViewController.swift
+++ PhotoPicker/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    @IBOutlet weak var img: UIImageView!
+    private var tapButton = UIButton()
+    var imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Counter"
+        imagePicker.delegate=self
+       
+        view.addSubview(tapButton)
+        tapButton.backgroundColor = .blue
+               tapButton.setTitle("TAP", for: .normal)
+               tapButton.translatesAutoresizingMaskIntoConstraints = false
+               
+               NSLayoutConstraint.activate([
+                   
+                   tapButton.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 30),
+                   tapButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+               ])
         // Do any additional setup after loading the view.
     }
-
+    override func  didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func onClickPickerImage(_ sender:Any){
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+        }
 
 }
 
