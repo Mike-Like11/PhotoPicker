2cb2d59513c777a07d3e11942dde8e7f01ffbd01
Mike-Like11 <mike7677877@gmail.com>
2021-03-11 18:59:13 +0300

    Initial Commit

---
+++ PhotoPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoPicker
+//
+//  Created by Student on 11.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
